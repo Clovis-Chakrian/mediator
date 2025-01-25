51c7cd553ad23ac93193433fa05197fdee7e1ac0
Clóvis Chakrian <clovischakriandev@gmail.com>
2025-01-25 12:56:55 -0300

    fix

--- src/main/java/com/clovischakrian/mediator/exceptions/FailedToHandleRequestException.java
+++ src/main/java/com/clovischakrian/mediator/exceptions/FailedToHandleRequestException.java
@@ -1,7 +1,14 @@
 package com.clovischakrian.mediator.exceptions;
 
 public class FailedToHandleRequestException extends RuntimeException {
+    private Throwable throwable;
+
     public FailedToHandleRequestException(String message, Throwable throwable) {
         super(message, throwable);
+        this.throwable = throwable;
+    }
+
+    public Throwable getThrowable() {
+        return throwable;
     }
 }
